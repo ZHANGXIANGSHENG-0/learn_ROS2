b7f737f8602466339726822ec6658cf94f4e203d
sam <san2223345@gmail.com>
2026-04-21 17:38:56 +0800

    建立雙海龜避障專案

---
+++ src/two_turtle_avoidance/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'two_turtle_avoidance'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sam',
+    maintainer_email='san2223345@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'two_turtle_avoidance = two_turtle_avoidance.two_turtle_avoidance:main'
+        ],
+    },
+)
---
+++ src/two_turtle_avoidance/two_turtle_avoidance/two_turtle_avoidance.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from two_turtle_avoidance.')
+
+
+if __name__ == '__main__':
+    main()
